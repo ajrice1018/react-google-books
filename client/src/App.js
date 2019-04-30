f236d64d23123b5bba2b3a810a6ad23a14153bce
Andrew Rice <andrewrice@Andrews-MacBook.hsd1.wa.comcast.net>
2019-04-30 12:12:48 -0700

    Updated App.js and relevant CSS files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Search}/>
+            <Route path="/saved" component={Saved}/>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
